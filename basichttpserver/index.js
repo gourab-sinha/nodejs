06389b06f98baa4b2bebff14ff8c05d9b45b4d4c
gourab-sinha <gourab19964u@gmail.com>
2020-12-07 11:09:20 +0530

    index.html page rendering with node server

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,12 +1,22 @@
 const http = require('http');
 const port = 8000;
-
+const fs = require('fs'); 
 
 // request handler
 function requestHandler(req, res){
     console.log(req.url);
     res.writeHead(200, {'content-type': 'text/html'});
-    res.end("<h1>Gotch!</h1>");
+    // fs.readFile('index.html', function(err, data){ also works
+    fs.readFile('./index.html', function(err, data){
+        if(err){
+            console.log('Error', err);
+            return res.end('<h1>Error</h1>');
+        }
+        
+        return res.end(data);
+    });
+    
+    // res.end("<h1>Gotch!</h1>");
 }
 
 const server = http.createServer(requestHandler);
